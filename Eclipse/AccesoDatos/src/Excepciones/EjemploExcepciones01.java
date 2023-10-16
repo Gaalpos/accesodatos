57dc3e18e04f89253fa3d0a367d9bca22255fb12
gaalpos <gabripazos13@gmail.com>
2023-10-16 18:07:14 +0200

    all

---
+++ Eclipse/AccesoDatos/src/Excepciones/EjemploExcepciones01.java
@@ -0,0 +1,9 @@
+package Excepciones;
+
+public class EjemploExcepciones01 {
+	public static void main(final String[] args) {
+		int numerador = 10, denominador = 0, cociente;
+		cociente = numerador / denominador;
+		System.out.println("Resultado = " + cociente);
+	}
+}
